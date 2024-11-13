1036413c1e45f4113a534c8f5df5dc01c9de2a01
ales-sandrito <calivaalessandro@gmail.com>
2024-11-13 18:09:53 +0200

    setup init

--- Skaiciuotuvas/calculator.py
+++ Skaiciuotuvas/calculator.py
@@ -59,3 +59,4 @@ while True:
         break
     else:
         print("Invalid input! Please choose a valid option.")
+
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Skaiciuotuvas",
+    version="0.1.1",
+    author_email="ales-sandrito",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.6"
+)
